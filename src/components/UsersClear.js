be119bafde35cb56c16f4ba1c8f0aa49e21e7296
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-03-20 12:24:25 +0100

    Added search users in AD

--- src/components/UsersClear.js
+++ src/components/UsersClear.js
@@ -2,55 +2,65 @@ import React from "react";
 import Select from "react-select";
 import fetch from "isomorphic-fetch";
 import "react-select/dist/react-select.css";
+import "./UserClear.css";
+import axios from "axios";
+
+const API_ENDPOINT = "http://10.24.14.148";
 
 class UsersClear extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			backspaceRemoves: true,
-			multi: false,
-			creatable: true
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      backspaceRemoves: true,
+      multi: false,
+      creatable: true
+    };
+  }
 
-	onChange = (value) => {
-		this.setState({
-			value: value
-		});
-	};
+  onChange = value => {
+    this.setState({
+      value: value
+    });
+  };
 
-	getUsers = (input) => {
-		if (!input) {
-			return Promise.resolve({ options: [] });
-		}
+  getUsers = input => {
+    if (!input) {
+      return Promise.resolve({ options: [] });
+    }
 
-		return fetch(`https://api.github.com/search/users?q=${input}`).then((response) => response.json()).then((json) => {
-			return { options: json.items };
-		});
-	};
+    // return fetch(`https://api.github.com/search/users?q=${input}`)
+    return axios
+      .get(`${API_ENDPOINT}/users/searchAD/${input}`)
+      .then(response => {
+        console.log(response.data.dtoObjects);
+        return { options: response.data.dtoObjects };
+      });
+  };
 
-	gotoUser = (value, event) => {
-		window.open(value.html_url);
-	};
+  // gotoUser = (value, event) => {
+  //   window.open(value.html_url);
+  // };
 
-	render() {
-		const AsyncComponent = this.state.creatable ? Select.AsyncCreatable : Select.Async;
-		console.log(this.state.value);
-		return (
-			<div className="section">
-				<h3 className="section-heading">{this.props.label} </h3>
-				<AsyncComponent
-					multi={this.state.multi}
-					value={this.state.value}
-					onChange={this.onChange}
-					valueKey="id"
-					labelKey="login"
-					loadOptions={this.getUsers}
-					backspaceRemoves={this.state.backspaceRemoves}
-				/>
-			</div>
-		);
-	}
+  render() {
+    const AsyncComponent = this.state.creatable
+      ? Select.AsyncCreatable
+      : Select.Async;
+    console.log(this.state.value);
+    return (
+      <div className="section">
+        <h3 className="section-heading">React-select</h3>
+        <AsyncComponent
+          multi={this.state.multi}
+          value={this.state.value}
+          onChange={this.onChange}
+          valueKey="lastName"
+          labelKey="fullName"
+          loadOptions={this.getUsers}
+          backspaceRemoves={this.state.backspaceRemoves}
+        />
+      </div>
+    );
+  }
 }
 
 export default UsersClear;
