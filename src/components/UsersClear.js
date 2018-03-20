bab075ea667ba531fc0703dce7cf69e5552389f9
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-03-20 15:39:09 +0100

    Done

--- src/components/UsersClear.js
+++ src/components/UsersClear.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Select from "react-select";
-import fetch from "isomorphic-fetch";
+// import fetch from "isomorphic-fetch";
 import "react-select/dist/react-select.css";
 import "./UserClear.css";
 import axios from "axios";
@@ -23,12 +23,15 @@ class UsersClear extends React.Component {
     });
   };
 
+  handleClick = () => {
+    alert(this.state.value.fullName);
+  };
+
   getUsers = input => {
     if (!input) {
       return Promise.resolve({ options: [] });
     }
 
-    // return fetch(`https://api.github.com/search/users?q=${input}`)
     return axios
       .get(`${API_ENDPOINT}/users/searchAD/${input}`)
       .then(response => {
@@ -36,10 +39,6 @@ class UsersClear extends React.Component {
         return { options: response.data.dtoObjects };
       });
   };
-
-  // gotoUser = (value, event) => {
-  //   window.open(value.html_url);
-  // };
 
   render() {
     const AsyncComponent = this.state.creatable
@@ -53,11 +52,12 @@ class UsersClear extends React.Component {
           multi={this.state.multi}
           value={this.state.value}
           onChange={this.onChange}
-          valueKey="lastName"
+          // valueKey="lastName"
           labelKey="fullName"
           loadOptions={this.getUsers}
           backspaceRemoves={this.state.backspaceRemoves}
         />
+        {this.state.value && <button onClick={this.handleClick}>Dalej</button>}
       </div>
     );
   }
